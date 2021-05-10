80f84a99ea787615faa1598327c0644226b54e39
arm-B <79102271+arm-B@users.noreply.github.com>
2021-05-10 21:19:33 +0900

    로또의 최고 순위와 최저 순위

--- lotto.java
+++ lotto.java
@@ -4,26 +4,27 @@ class Solution {
         int[] answer = new int[2];
         
         for (int i = 0; i<lottos.length;i++) {
-			if(lottos[i] == 0)
-				max++;
-			else
-				for(int j = 0; j<win_nums.length; j++) {
-					if(lottos[i]==win_nums[j]) {
-						min++;
-						max++;
-						break;
-					}
+		if(lottos[i] == 0)		//모르는 번호 최고 등수인 max만 하나 추가
+			max++;
+		else
+			for(int j = 0; j<win_nums.length; j++) {	//그 외의 값인 경우 중복 없으니까 확인 없이
+				if(lottos[i]==win_nums[j]) {		//동일한 값이 있으면 min과 max 모두 하나씩 추가
+					min++;
+					max++;
+					break;
 				}
-		}
-		if(min <= 1)
-			answer[1] = 6;
-		else
-			answer[1] = 7-min;
+			}
+	}
+	//귀찮은 나머지... swith-case문 안 쓰고 낙첨 제외 7-맞힌 개수로 등수를 구했습니다.
+	if(min <= 1)
+		answer[1] = 6;
+	else
+		answer[1] = 7-min;
 		
-		if(max <= 1)
-			answer[0] = 6;
-		else
-			answer[0] = 7-max;
+	if(max <= 1)
+		answer[0] = 6;
+	else
+		answer[0] = 7-max;
         
         return answer;
     }
