33522fe62c749742424b8fa409bc44ce828e61e7
arm-B <79102271+arm-B@users.noreply.github.com>
2021-05-10 21:31:10 +0900

    음양 더하기

--- eumyang.java
+++ eumyang.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-      //signs의 값따라 true면 더해주고 false면 빼주기
-      //음... 너무 간단한가
-		for(int i = 0; i<absolutes.length;i++) {
-			if(signs[i])
-				answer += absolutes[i];
-			else
-				answer -= absolutes[i];
-		}
+	//signs의 값따라 true면 더해주고 false면 빼주기
+	//음... 너무 간단한가
+	for(int i = 0; i<absolutes.length;i++) {
+		if(signs[i])
+			answer += absolutes[i];
+		else
+			answer -= absolutes[i];
+	}
         return answer;
     }
 }
