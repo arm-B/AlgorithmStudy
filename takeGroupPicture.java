64bf02fc051ab139d1683a0f8911990b8a684f6a
arm-B <79102271+arm-B@users.noreply.github.com>
2021-05-19 18:01:37 +0900

    단체사진 찍기

--- takeGroupPicture.java
+++ takeGroupPicture.java
@@ -1,60 +1,60 @@
 import java.util.*;
 class Solution {
     static List<Character> position;
-	static char[] kko = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-	static int answer;
-	static boolean[] visited;
+    static char[] kko = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    static int answer;
+    static boolean[] visited;
     
     public int solution(int n, String[] data) {
         visited = new boolean[kko.length];
-		position = new ArrayList<Character>();
+	position = new ArrayList<Character>();
         for(int i = 0; i<kko.length; i++) {
-			position.add('0');
-		}
-		answer = 0;
+		position.add('0');
+	}
+	answer = 0;
         countCase(0, n, data);
         return answer;
     }
     
     static boolean isTrue(String[] data) {
-		for(int i = 0; i<data.length;++i) {
-			char[] want = data[i].toCharArray();
-			int start = 0, end = 0;
+	for(int i = 0; i<data.length;++i) {
+		char[] want = data[i].toCharArray();
+		int start = 0, end = 0;
 			
-			int gap = Math.abs(position.indexOf(want[0])-position.indexOf(want[2]));
+		int gap = Math.abs(position.indexOf(want[0])-position.indexOf(want[2]));
 			
-			switch(want[3]) {
-				case '>':
-					if(gap <= want[4]-'0'+1)
-						return false;
-					break;
-				case '=' :
-					if(gap != want[4]-'0'+1)
-						return false;
-					break;
-				case '<' :
-					if(gap >= want[4]-'0'+1)
-						return false;
+		switch(want[3]) {
+			case '>':
+				if(gap <= want[4]-'0'+1)
+					return false;
 				break;
-			}
+			case '=' :
+				if(gap != want[4]-'0'+1)
+					return false;
+				break;
+			case '<' :
+				if(gap >= want[4]-'0'+1)
+					return false;
+			break;
 		}
-		return true;
 	}
-	static void countCase(int index, int n, String[] data) {
-		if(index == 8) {
-			if(isTrue(data)) {
-				answer++;
-			}
-			return;
+	return true;
+    }
+    static void countCase(int index, String[] data) {
+	if(index == 8) {
+		if(isTrue(data)) {
+			answer++;
 		}
+		return;
+	}
 		
-		for(int i= 0; i<kko.length;++i) {
-			if(!visited[i]) {
-				visited[i] = true;
-				position.set(index, kko[i]);
-				countCase(index+1, n, data);
-				visited[i] = false;
-			}
-		}		
-	}
+	for(int i= 0; i<kko.length;++i) {
+		if(!visited[i]) {
+			visited[i] = true;
+			position.set(index, kko[i]);
+			countCase(index+1, data);
+			visited[i] = false;
+		}
+	}		
+    }
 }
