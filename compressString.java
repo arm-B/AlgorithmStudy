41927b77b68fcaeb70f3a5629e17dc8f12df086c
arm-B <79102271+arm-B@users.noreply.github.com>
2021-05-19 17:39:40 +0900

    문자열압축

--- compressString.java
+++ compressString.java
@@ -2,32 +2,32 @@ class Solution {
     public int solution(String s) {
         int answer = s.length();
 		
-		for(int i = 1; i<=s.length()/2; i++) {
-			String compressed = compressString(s, i);
-			answer = Math.min(answer, compressed.length());
-		}
+	for(int i = 1; i<=s.length()/2; i++) {
+		String compressed = compressString(s, i);
+		answer = Math.min(answer, compressed.length());
+	}
         return answer;
     }
-	static String compressString(String s, int gap) {
-		int count = 1;
-		StringBuilder sb = new StringBuilder();
-		String check = "";
-		for(int i = 0; i<s.length(); i += gap) {
-			if(s.length()-i >= gap)
-				check = s.substring(i, i+gap);
-			else
-				check = s.substring(i);
+    static String compressString(String s, int gap) {
+	int count = 1;
+	StringBuilder sb = new StringBuilder();
+	String check = "";
+	for(int i = 0; i<s.length(); i += gap) {
+		if(s.length()-i >= gap)
+			check = s.substring(i, i+gap);
+		else
+			check = s.substring(i);
 			
-			if(i+gap+gap<=s.length() && check.equals(s.substring(i+gap, i+gap+gap))) {
-				count += 1;
-			}
-			else {
-				if(count > 1) sb.append(count);
-				sb.append(check);
-				check = "";
-				count = 1;
-			}
+		if(i+gap+gap<=s.length() && check.equals(s.substring(i+gap, i+gap+gap))) {
+			count += 1;
 		}
-        return sb.toString();
+		else {
+			if(count > 1) sb.append(count);
+			sb.append(check);
+			check = "";
+			count = 1;
+		}
 	}
+        return sb.toString();
+    }
 }
