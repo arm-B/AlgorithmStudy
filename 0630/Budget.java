79eb5937ec479c2730d55a2c829973ccf9765c9a
arm-B <79102271+arm-B@users.noreply.github.com>
2021-06-30 15:10:09 +0900

    예산

---
+++ 0630/Budget.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+  public int solution(int[] d, int budget) {
+    int answer = 0;
+    int index = 0;
+    
+		Arrays.sort(d);
+    while(budget>0 && index < d.length) {
+      if(budget-d[index]>=0) {
+        budget-=d[index++];
+        answer++;
+      }
+      else
+        break;
+    }
+    return answer;
+  }
+}
