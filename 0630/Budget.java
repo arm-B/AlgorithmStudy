7761fefcdad79eea9eff4ddb6a8b8811f2f1384d
arm-B <79102271+arm-B@users.noreply.github.com>
2021-06-30 20:23:29 +0900

    예산

--- 0630/Budget.java
+++ 0630/Budget.java
@@ -4,7 +4,7 @@ class Solution {
     int answer = 0;
     int index = 0;
     
-		Arrays.sort(d);
+    Arrays.sort(d);
     while(budget>0 && index < d.length) {
       if(budget-d[index]>=0) {
         budget-=d[index++];
