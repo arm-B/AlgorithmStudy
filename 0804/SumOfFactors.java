23d697e64b04202c40476d231794e19d12712180
arm-B <79102271+arm-B@users.noreply.github.com>
2021-08-03 20:57:48 +0900

    약수의 합

--- 0804/SumOfFactors.java
+++ 0804/SumOfFactors.java
@@ -4,14 +4,13 @@ class Solution {
     
     if(n == 0)
       answer = 0;
-    else if(n==1)
+    else if(n == 1)
       answer = 1;
-    else{
-      for(int i = 2; i<=n/2+1; i++) {
+    else
+      for(int i = 2; i <= n/2 + 1; i++)
         if(n%i == 0)
           answer += i;
-      }
-    }
+    
     return answer;
   }
 }
